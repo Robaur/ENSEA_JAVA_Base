eb6d1aec87d3d0feae09ba1c7cb33d732069a957
Robaur <auriac.robin.pro@gmail.com>
2023-10-16 16:12:31 +0200

    base

---
+++ TD/Place.java
@@ -0,0 +1,59 @@
+//------------------------------------------------
+
+// THis game is a direct application of the ENSEA java's base
+
+
+
+
+//------------------------------------------------
+
+public class Place {
+
+
+    public Place(void){
+        this.Up=NULL;
+        this.Down=NULL;
+        this.Left=NULL;
+        this.Right=NULL;
+    }
+
+
+    public Place(Place[] Neighbor,int[] place){
+            for (int k=0; k<4;k++){
+            }
+    }
+
+
+
+
+ //----------------- ALL METHODE TO CHANGE ATTRIBUTE ----------------------------------------------
+
+    public Change_UP (Place New){
+        this.Up=New;
+    }
+
+    public Change_Down (Place New){
+        this.Down=New;
+    }
+
+    public Change_Left (Place New){
+        this.Left=New;
+    }
+    public Change_Right (Place New){
+        this.Right=New;
+    }
+
+    public Change_ground_type(int New){
+        this.ground_type=New;
+    }
+
+ //-----------------   ATTRIBUTE -------------------------------------------------------------------
+
+    private Place Up;
+    private Place Down;
+    private Place Left;
+    private Place Right;
+
+    private int ground_type;
+
+}
---
+++ TD/main.java
@@ -0,0 +1,24 @@
+//------------------------------------------------
+
+// THis game is a direct application of the ENSEA java's base
+
+
+
+
+//------------------------------------------------
+public class static main{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
