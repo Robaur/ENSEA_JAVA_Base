1082b7fe5d6d2836e95229c21a59541a0729bb35
EstelleTournassat <estelle.tournassat@ensea.fr>
2023-11-24 17:32:54 +0100

    Unit

--- RTS/Unit.java
+++ RTS/Unit.java
@@ -9,33 +9,85 @@ public class Unit extends Entity {
     private int Range;
     private Entity Target;
 
-    public Unit(int Attack, int Lane, int Range, Entity Target) {
+    public Unit(int Attack, int Lane, int Range, Entity Target,int x,int y,str name,int PV,int team) {
         this.Attack = Attack;
         this.Lane = Lane;
         this.Range = Range;
         this.Target = Target;
-        this.x=0;
-        this.y=0;
-        this.name="";
-        this.PV=100;
+        this.x=x;
+        this.y=y;
+        this.name=name;
+        this.PV=PV;
+        this.team=team;
         //ajouter les variables de entity dedans
     }
 
-    public void unit_move (x, y,Target){
-        int x_final=Target.x;
-        int y_final=Target.y;
+    public void unit_move (void){
+        int x_target=unit_detect()(1);
+        int y_target=unit_detect()(2);
         Random ran = new Random();
         Lane = ran.nextInt(3);
-        if (Lane==0) {
+        
+        while (x == null || y== null){
+        switch (team){
+            case 1:
+            int x_limit= 100 ;//coordonnée x max de la map
+            int y_limit=0;
+            switch (Lane){
+                case 0:
+                if(this.y >y_limit){
+                    this.y--;
+                }
+                else if (this.x<x_limit){
+                    this.x++;
+                }
+                break;
+
+                case 1:
+                if(this.x<x_limit){this.x++;}
+                else if (this.y >y_limit){this.y--;}
+
+                default:
+                if (this.x<x_limit && this.y >y_limit){
+                this.y--;
+                this.x++;
+                }
+                else if(this.x<x_limit && this.y ==y_limit){this.x++;}
+                else if(this.x==x_limit && this.y >y_limit){ this.y--;}
+                break;
+            }
+            break;
+
+            default:
+            int x_limit=0 ;
+            int y_limit=100;//coordonnée y max de la map
+            switch (Lane){
+                case 0:
+                if(this.y <y_limit){this.y++;}
+                else if (this.x>x_limit){this.x--;}
+                break;
+
+                case 1:
+                if(this.x>x_limit){this.x--;}
+                else if (this.y <y_limit){this.y++;}
+
+                default:
+                if (this.x>x_limit && this.y <y_limit){
+                this.y++;
+                this.x--;
+                }
+                else if(this.x>x_limit && this.y ==y_limit){this.x--;}
+                else if(this.x==x_limit && this.y <y_limit){this.y++;}
+                break;
+            }
+            break;
 
         }
-        else if (Lane==1) {
-            // si pas de bord: on fait x=x+1 sinon on fait y=y-1
-        }
-        else {
-        }
-        // code pour déplacer une unité sur une case en fonction du target
+        x_target=unit_detect()(1);
+        y_target=unit_detect()(2);
     }
+    
+}
 
     public void unit_fight (Target) throws InterruptedException {
         while(this.PV>0) {
@@ -53,7 +105,5 @@ public class Unit extends Entity {
     }
 
     public void unit_detect(){
-        if
-        }
     }
 }
